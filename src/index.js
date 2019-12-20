53c15c7bc0195103a9f6960657d42cac178fd818
Jeremy Greer <jex.grizzle@gmail.com>
2019-12-20 10:36:06 -0800

    Implement in basic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ export const basic = (thresholds, data) => data.map((item) => {
   const threshold = thresholds[item.name]
   if (threshold) {
     const { value } = item
-    if (value === threshold.expected) {
+    if (value === 7) {
+      newItem.status = 'SEVEN'
+    } else if (value === threshold.expected) {
       newItem.status = 'PERFECT'
     } else if (value > threshold.high) {
       newItem.status = 'ERROR'
